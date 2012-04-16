4e4b96fcd4116d2396df70a6d736604deb27fed1
Adrian Mowat <adrian.mowat@gmail.com>
2012-04-16 15:28:49 +0100

    Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MettleMvp::Application.config.secret_token = 'b19397275f9453296c5d6a943749c9e941fb48162ea9db47bad125cd6b95383d4804898bf9fbee92986eebab94606f59a66558a044395b86f5975a885af66311'
